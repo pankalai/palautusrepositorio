8a21f85a0843d2f1e9155b2ef49e7e1f122bbcaf
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-11-30 18:47:13 +0200

    viikko 5

---
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -0,0 +1,143 @@
+from enum import Enum
+from tkinter import ttk, constants, StringVar
+
+
+class Komento(Enum):
+    SUMMA = 1
+    EROTUS = 2
+    NOLLAUS = 3
+    KUMOA = 4
+
+
+class Kayttoliittyma:
+    def __init__(self, sovellus, root):
+        self._sovellus = sovellus
+        self._root = root
+        self.komento_oliot = []
+        self._komennot = {
+            Komento.SUMMA: Summa(sovellus, self._lue_syote),
+            Komento.EROTUS: Erotus(sovellus, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(sovellus),
+            Komento.KUMOA: Kumoa(
+                sovellus,
+                self.komento_oliot,
+            ),
+        }
+
+    def kaynnista(self):
+        self._arvo_var = StringVar()
+        self._arvo_var.set(self._sovellus.arvo())
+        self._syote_kentta = ttk.Entry(master=self._root)
+
+        tulos_teksti = ttk.Label(textvariable=self._arvo_var)
+
+        summa_painike = ttk.Button(
+            master=self._root,
+            text="Summa",
+            command=lambda: self._suorita_komento(Komento.SUMMA),
+        )
+
+        erotus_painike = ttk.Button(
+            master=self._root,
+            text="Erotus",
+            command=lambda: self._suorita_komento(Komento.EROTUS),
+        )
+
+        self._nollaus_painike = ttk.Button(
+            master=self._root,
+            text="Nollaus",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.NOLLAUS),
+        )
+
+        self._kumoa_painike = ttk.Button(
+            master=self._root,
+            text="Kumoa",
+            state=constants.DISABLED,
+            command=lambda: self._suorita_komento(Komento.KUMOA),
+        )
+
+        tulos_teksti.grid(columnspan=4)
+        self._syote_kentta.grid(columnspan=4, sticky=(constants.E, constants.W))
+        summa_painike.grid(row=2, column=0)
+        erotus_painike.grid(row=2, column=1)
+        self._nollaus_painike.grid(row=2, column=2)
+        self._kumoa_painike.grid(row=2, column=3)
+
+    def _lue_syote(self):
+        return int(self._syote_kentta.get())
+
+    def _suorita_komento(self, komento):
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
+        if komento != Komento.KUMOA:
+            self.komento_oliot.append(komento_olio)
+
+        self._kumoa_painike["state"] = constants.NORMAL
+
+        if self._sovellus.arvo() == 0:
+            self._nollaus_painike["state"] = constants.DISABLED
+        else:
+            self._nollaus_painike["state"] = constants.NORMAL
+
+        if not self.komento_oliot:
+            self._kumoa_painike["state"] = constants.DISABLED
+        else:
+            self._kumoa_painike["state"] = constants.NORMAL
+
+        self._syote_kentta.delete(0, constants.END)
+        self._arvo_var.set(self._sovellus.arvo())
+
+
+class Summa:
+    def __init__(self, io, syote):
+        self.io = io
+        self.syote = syote
+        self.arvo = []
+
+    def suorita(self):
+        self.arvo.append(self.io.arvo())
+        self.io.plus(self.syote())
+
+    def kumoa(self):
+        self.io.aseta_arvo(self.arvo.pop())
+
+
+class Erotus:
+    def __init__(self, io, syote):
+        self.io = io
+        self.syote = syote
+        self.arvo = []
+
+    def suorita(self):
+        self.arvo.append(self.io.arvo())
+        self.io.miinus(self.syote())
+
+    def kumoa(self):
+        self.io.aseta_arvo(self.arvo.pop())
+
+
+class Nollaus:
+    def __init__(self, io):
+        self.io = io
+        self.arvo = []
+
+    def suorita(self):
+        self.arvo.append(self.io.arvo())
+        self.io.nollaa()
+
+    def kumoa(self):
+        self.io.aseta_arvo(self.arvo.pop())
+
+
+class Kumoa:
+    def __init__(self, io, ed):
+        self.io = io
+        self.komennot = ed
+
+    def suorita(self):
+        if not self.komennot:
+            self.io.aseta_arvo(0)
+            return
+        ed = self.komennot.pop()
+        ed.kumoa()
